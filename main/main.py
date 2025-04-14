f51df28cd76a767740611a4d11db989c3d0dab83
Rohan Rodrigues.J <rodriguz3071@gmail.com>
2025-04-14 16:26:04 +0530

    updated main_pc

--- main/main.py
+++ main/main.py
@@ -154,11 +154,10 @@ def fetch_text_from_camera(_=None):
         image_b64 = encode_image(image)
 
         response = send_request("ocr_detect", "INLINE", {"image_b64": image_b64})
-        if response and response['status'] == "SUCCESS":
-            logging.info(f"Received from OCR {response}")
+        logging.info(f"Received from OCR {response}")
+        if response and response['status'] == "SUCCESS" and response['data']:
             tts_response = send_request("text_to_speech", "INLINE", {"text": response['data']})
             if tts_response and tts_response['status'] == "SUCCESS":
-                logging.info(f"Received from tts {tts_response['status']}")
                 decode_and_play_audio(tts_response['data'])
             else:
                 logging.error(f"TTS failed: {tts_response}")
